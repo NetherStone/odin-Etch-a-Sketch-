ea2b1533731f5a742b1544f4692b6a45fde0e9bc
Regan <reganpuah@gmail.com>
2023-12-06 17:30:10 +0800

    Added class name to style the grids.

--- js-file.js
+++ js-file.js
@@ -3,6 +3,7 @@ const gridContainer = document.getElementById("gridContainer");
 for (let i = 0; i < 16; i++){ //16x16 grid in js
     for (let x = 0; x < 16; x++) {
         let gridDiv = document.createElement("div");
+        gridDiv.className = "gridDiv";
         gridContainer.appendChild(gridDiv);
     }
 }
