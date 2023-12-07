d4a3a5eb5bd1f3c9b34d26ebe41738a98c6800a9
Regan <reganpuah@gmail.com>
2023-12-07 10:48:43 +0800

    Click and drag effect added.

--- js-file.js
+++ js-file.js
@@ -5,9 +5,22 @@ for (let i = 0; i < 16; i++){ //16x16 grid in js
         let gridDiv = document.createElement("div");
         gridDiv.className = "gridDiv";
 
-        gridDiv.addEventListener("mouseover", () => {
-            gridDiv.classList.add("hovered");
+        gridDiv.addEventListener("mousedown", () => {
+            isMouseDown = true;
         });
+
+        gridDiv.addEventListener("mouseup", () => {
+            isMouseDown = false;
+        });
+
+        gridDiv.addEventListener("mouseenter", () => {
+            if (isMouseDown) {
+                gridDiv.classList.add("hovered")
+            }
+        });
+       /*gridDiv.addEventListener("mouseover", () => {
+            gridDiv.classList.add("hovered");
+        }); */
 
         gridContainer.appendChild(gridDiv);
     }
