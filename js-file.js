8b8898878f607143eb0d3e7271c5379314e170c6
Regan <reganpuah@gmail.com>
2023-12-13 10:21:45 +0800

    Moved variables above.

--- js-file.js
+++ js-file.js
@@ -2,23 +2,25 @@ const gridContainer = document.getElementById("gridContainer");
 
 const squaresButton = document.getElementById("squaresButton");
 
-let userNumber = 64
+let userNumber = 16
+
+let isMouseDown = false;
 
 squaresButton.addEventListener("click", () =>{ //Ask user for number of squares in grid, number should range from 1 to 100.
      userNumber = prompt("Please enter the number of squares you want in the grid.");
      userNumber = Math.floor(userNumber);
      if (isNaN(userNumber)){
-        userNumber = 64;
+        userNumber = 16;
      }
+
      if (userNumber < 1){
-        userNumber = 64;
+        userNumber = 16;
      } else if (userNumber > 100){
-        userNumber = 64;
+        userNumber = 16;
      }
+
      console.log(userNumber);
 });
-
-let isMouseDown = false;
 
 for (let i = 0; i < 16; i++){ //16x16 grid in js
     for (let x = 0; x < 16; x++){
