5255e897060922ce3b03ba17851444bad9e7748e
Regan <reganpuah@gmail.com>
2023-12-13 12:43:57 +0800

    Moved div creating block above.

--- js-file.js
+++ js-file.js
@@ -6,22 +6,6 @@ let userNumber = 16
 
 let isMouseDown = false;
 
-squaresButton.addEventListener("click", () =>{ //Ask user for number of squares in grid, number should range from 1 to 100.
-     userNumber = prompt("Please enter the number of squares you want in the grid.");
-     userNumber = Math.floor(userNumber);
-     if (isNaN(userNumber)){
-        userNumber = 16;
-     }
-
-     if (userNumber < 1){
-        userNumber = 16;
-     } else if (userNumber > 100){
-        userNumber = 16;
-     }
-
-     console.log(userNumber);
-});
-
 for (let i = 0; i < 16; i++){ //16x16 grid in js
     for (let x = 0; x < 16; x++){
         let gridDiv = document.createElement("div");
@@ -45,3 +29,19 @@ for (let i = 0; i < 16; i++){ //16x16 grid in js
         gridContainer.appendChild(gridDiv); //append
     }
 }
+
+squaresButton.addEventListener("click", () =>{ //Ask user for number of squares in grid, number should range from 1 to 100.
+     userNumber = prompt("Please enter the number of squares you want in the grid.");
+     userNumber = Math.floor(userNumber);
+     if (isNaN(userNumber)){
+        userNumber = 16;
+     }
+
+     if (userNumber < 1){
+        userNumber = 16;
+     } else if (userNumber > 100){
+        userNumber = 16;
+     }
+
+     console.log(userNumber);
+});
